71e8827dc9d2593209ac8d63a4e9b529da03275b
Abhiram Jayakumar <abhiramjayakumar07@gmail.com>
2026-08-19 15:41:39 +0530

    Update count_integral_points_inside_triangle.cpp

--- Triangle/count_integral_points_inside_triangle.cpp
+++ Triangle/count_integral_points_inside_triangle.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <numeric>
+#include <cmath>
 using namespace std;
 
 int main() {
@@ -10,6 +11,15 @@ int main() {
     cin >> x1 >> y1;
     cin >> x2 >> y2;
     cin >> x3 >> y3;
+
+    float ab = sqrt(pow(x2-x1,2)+pow(y2-y1,2));
+    float bc = sqrt(pow(x3-x2,2)+pow(y3-y2,2));
+    float ac = sqrt(pow(x3-x1,2)+pow(y3-y1,2));
+    
+    if (ab + bc == ac || ab + ac == bc || bc + ac == ab) {
+        cout << "Given points are collinear!\n";
+        return 0;
+    }
 
     int B = 0;
     B += gcd(abs(x1-x2), abs(y1-y2));
